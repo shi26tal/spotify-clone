1686dd79406d76eb8bdae022831402cc046383b8
Shital Prajapati <shital26prajapati@gmail.com>
2026-05-30 17:20:20 +0545

    testing4

--- api/deezer/[...path].js
+++ api/deezer/[...path].js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const { path } = req.query;
+    const { path = [] } = req.query;
 
     const deezerPath = Array.isArray(path)
       ? path.join("/")
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(response.status).send(data);
-  } catch (err) {
-    res.status(500).json({ error: "Proxy failed" });
+  } catch (e) {
+    res.status(500).json({ error: "proxy failed" });
   }
 }
